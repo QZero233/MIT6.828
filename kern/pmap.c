4da1d934f7baf86c7b6dc49e496c684d6e2b8673
qzero <qzero@NB.localdomain>
2024-03-07 19:48:32 +0800

    Finish load env

--- kern/pmap.c
+++ kern/pmap.c
@@ -187,6 +187,7 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs=(struct Env*)boot_alloc(sizeof(struct Env)*NENV);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -227,6 +228,10 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	for(physaddr_t pa=PADDR(envs),offset=0;offset<sizeof(struct Env)*NENV;pa+=PGSIZE,offset+=PGSIZE){
+		uintptr_t va=UENVS+offset;
+		*pgdir_walk(kern_pgdir,(void*)va,1)=pa|PTE_U|PTE_P;
+	}
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
